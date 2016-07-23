198d846c54c57bb644a7a220029e4ad71a9b539e
Matthew Attanasio <matthew.attanasio135@gmail.com>
2016-07-23 15:23:00 +1000

    some changes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
+//sets the port the port set by heroku, if not sets to 8080.
+var port=process.env.PORT || 8080;
+
+
+
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
 	console.log('Example app listening on port 3000!');
 });
