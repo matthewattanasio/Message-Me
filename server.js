64acc34f672166e34b8fbfff3175c1348d7fbc33
Joel Macey <101105702@student.swin.edu.au>
2016-07-23 15:00:54 +1000

    variable ports

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
 	console.log('Example app listening on port 3000!');
 });
