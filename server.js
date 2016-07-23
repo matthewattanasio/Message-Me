e3c65d15181273ed6401f89e90f6ef416565b259
Joel Macey <101105702@student.swin.edu.au>
2016-07-23 17:00:51 +1000

    im a loser

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var pg = require('pg');
 var app = express();
+var fs = require('fs');
+
+
+var indexTemplate = fs.readFileSync('index.html', 'utf8');
+
+
 
 //sets the port the port set by heroku, if not sets to 8080.
 var port=process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.send(indexTemplate);
 });
 
+app.use('/app', express.static('app'));
 
-//postgres
-
+//Postgres
 app.get('/items', function (request, res) {
   pg.connect( 'postgres://uffenisozqdsxl:vhQncKyzPtSGZj78ygXve9M_NJ@ec2-54-225-89-110.compute-1.amazonaws.com:5432/dfq5t551ivghpb?ssl=true', function(err, client, done) {
    console.log(err);
@@ -31,4 +37,3 @@ app.get('/items', function (request, res) {
 app.listen(port, function () {
 	console.log('Example app listening on port!' + port );
 });
-
