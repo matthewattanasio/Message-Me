73b2f68f31b77bf150c5ce4f0d8de94598e0f2c4
Matthew Attanasio <matthew.attanasio135@gmail.com>
2016-07-29 22:15:58 +1000

    fixed issue in package.json

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ if (process.env.NODE_ENV === 'development') {
   server.locals.isDevelopment = true;
 }
 
+if (process.env.NODE_ENV === 'production') {
+  config.port = 3000;
+  config.host = 'localhost';
+  server.locals.assetPath = 'http://localhost:8080/';
+  server.locals.isDevelopment = true;
+}
+
 const webServer = server.listen(config.port, config.host, function (err) {
   if (err) throw err;
   console.log('Web server listening at http://%s:%d', config.host, config.port);
